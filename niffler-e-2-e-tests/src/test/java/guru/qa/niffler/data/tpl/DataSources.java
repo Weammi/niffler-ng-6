6292a344fe46c90d2ea86d30b96662dc3db527b0
Weammi <endlessofgame@gmail.com>
2024-10-02 23:12:04 +0300

    HW-7: hw-7-3

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/tpl/DataSources.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/tpl/DataSources.java
@@ -15,11 +15,6 @@ public class DataSources {
     private DataSources() {
     }
 
-
-
-
-
-
     public static DataSource dataSource(String jdbcUrl) {
         return dataSources.computeIfAbsent(
                 jdbcUrl,
