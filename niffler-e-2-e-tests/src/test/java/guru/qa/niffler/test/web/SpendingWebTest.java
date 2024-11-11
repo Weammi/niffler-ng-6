5df498c04ed21a2e3e7cdf197f81af6ebff4fa5b
Weammi <endlessofgame@gmail.com>
2024-11-11 23:50:09 +0300

    HW-15: hw-15-1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -2,18 +2,25 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Date;
 
+import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.utils.RandomDataUtils.randomCategoryName;
 import static guru.qa.niffler.utils.RandomDataUtils.randomSentence;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 class SpendingWebTest {
@@ -60,6 +67,25 @@ class SpendingWebTest {
                 .checkAlert("New spending is successfully created");
 
         new MainPage().checkThatTableContainsSpending(description);
+    }
+
+    @User(
+            spendings = @Spending(
+                    category = "Обучение",
+                    description = "Обучение Advanced 2.0",
+                    amount = 79990
+            )
+    )
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.testData().password());
+
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 }
 
